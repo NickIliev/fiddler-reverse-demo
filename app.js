872ac59702b7ea4cf38b92665fba9cfc2bc8ee4c
Nikolay Iliev <nikolay.iliev@progress.com>
2025-06-26 08:28:32 +0100

    update demo

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const xmlHttp = new XMLHttpRequest();
 try {
-    // The application uses custom port 8866 - this is the port on which the Fiddler proxy works
+    /* T
+    The default server port is 8843 (when Fiddler is not in the reverse proxy mode the client app should use that port).
+    However, in this case, we must set port 8877, which corresponds to the "Client Port" value within the "Reverse Proxy" section of the Fiddler app
+    */
+    xmlHttp.open("GET", 'http://localhost:8877/jsonData', false);
+
     xmlHttp.setRequestHeader('content-type', 'application/json');
-    xmlHttp.open("GET", 'http://localhost:8866/jsonData', false);
     xmlHttp.setRequestHeader('custom-header', 'Nick Iliev');
+
     xmlHttp.send(null);
     result = xmlHttp.responseText;
     console.log(`Result: ${result}`);
